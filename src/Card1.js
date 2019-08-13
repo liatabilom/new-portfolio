f31a70c071835363b1693ec118a1812ca84053a1
Lia <lia.tabilo@gmail.com>
2019-08-13 21:07:30 +0100

    Add spinner to carousel

--- src/Card1.js
+++ src/Card1.js
@@ -1,33 +1,46 @@
 import React, { Component } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Loader from "react-loader-spinner";
 
 class Card1 extends Component {
+  state = {
+    loaded: false
+  };
+
+  carousel = () => {
+    if (this.state.loaded === true) {
+      return (
+        <div className="carousel">
+          <Carousel autoPlay={true} showThumbs={false} showStatus={false}>
+            <div>
+              <img
+                src="https://source.unsplash.com/jO3OYLdcxJo/200x100"
+                alt="First slide"
+              />
+            </div>
+            <div>
+              <img
+                src="https://source.unsplash.com/o4Tc9wRMVVM/200x100"
+                alt="Second slide"
+              />
+            </div>
+            <div>
+              <img
+                src="https://source.unsplash.com/xEY0dtFfon0/200x100"
+                alt="Third"
+              />
+            </div>
+          </Carousel>
+        </div>
+      );
+    } else {
+      return <Loader type="Puff" color="#00000" height="100" width="100" />;
+    }
+  };
+
   render() {
-    return (
-      <div className="carousel">
-        <Carousel autoPlay={true} showThumbs={false} showStatus={false}>
-          <div>
-            <img
-              src="https://source.unsplash.com/jO3OYLdcxJo/200x100"
-              alt="First slide"
-            />
-          </div>
-          <div>
-            <img
-              src="https://source.unsplash.com/o4Tc9wRMVVM/200x100"
-              alt="Second slide"
-            />
-          </div>
-          <div>
-            <img
-              src="https://source.unsplash.com/xEY0dtFfon0/200x100"
-              alt="Third"
-            />
-          </div>
-        </Carousel>
-      </div>
-    );
+    return <div>{this.carousel()}</div>;
   }
 }
 
